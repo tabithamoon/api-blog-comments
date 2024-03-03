3a639b15382eb1c0223ea35dc348cc34300e64ea
Tabitha <123581598+tabithamoon@users.noreply.github.com>
2024-03-03 20:39:49 -0300

    Ditch application/json header (thanks JavaScript)

--- src/index.js
+++ src/index.js
@@ -39,7 +39,9 @@ app.get('/key', async (c) => {
 
     // boot them out if that is the case. don't spam!
     if(addr !== null)
-        return c.text('Too many requests', 429)
+        return c.text('Too many requests', 429, {
+            'Access-Control-Allow-Origin': 'https://tabby.page'
+        })
 
     // if they ain't spamming, generate a fresh key for our fine user
     const newKey = await crypto.randomUUID()
@@ -54,7 +56,9 @@ app.get('/key', async (c) => {
     await c.env.KEYS.put(newKey, JSON.stringify(data), {expirationTtl: 90})
 
     // return it to the frontend
-    return c.text(newKey, 200)
+    return c.text(newKey, 200, {
+        'Access-Control-Allow-Origin': 'https://tabby.page'
+    })
 })
 
 app.post('/new/:slug', async (c) => {
@@ -67,19 +71,20 @@ app.post('/new/:slug', async (c) => {
 
     // try to parse body as JSON
     // catch error if it fails and return a bad request code
-    try {
-        if (c.req.header('Content-Type') !== "application/json")
-            throw new Error()
-        
-        req = await c.req.json()
+    try {       
+        req = JSON.parse(await c.req.text())
     }
     catch {
-        return c.text('Bad Request', 400)
+        return c.text('Bad Request', 400, {
+            'Access-Control-Allow-Origin': 'https://tabby.page'
+        })
     }
-    
+
     // Return unauthorized if author or body are too long
-    if(req.body.length() > 512 || req.author.length() > 32)
-        return c.text("Content too long", 400)
+    if(req.body.length > 512 || req.author.length > 32)
+        return c.text("Content too long", 400, {
+            'Access-Control-Allow-Origin': 'https://tabby.page'
+        })
 
     // get key and IP address info
     const key = JSON.parse(await c.env.KEYS.get(req.key))
@@ -87,15 +92,21 @@ app.post('/new/:slug', async (c) => {
 
     // boot them out if they don't have a nice and fresh key
     if(key === null)
-        return c.text('Unauthorized', 401)
+        return c.text('Unauthorized', 401, {
+            'Access-Control-Allow-Origin': 'https://tabby.page'
+        })
 
     // boot them out if their address doesn't match the key's origin
     if(key.source !== requestIP)
-        return c.text('Unauthorized', 401)
+        return c.text('Unauthorized', 401, {
+            'Access-Control-Allow-Origin': 'https://tabby.page'
+        })
 
     // boot them out if they're trying to spam. naughty!
     if(addr !== null)
-        return c.text('Too many requests', 429)
+        return c.text('Too many requests', 429, {
+            'Access-Control-Allow-Origin': 'https://tabby.page'
+        })
 
     // check if the requested page exists
     const pagecheck = await c.env.DB.prepare(
@@ -108,7 +119,9 @@ app.post('/new/:slug', async (c) => {
     ).bind(slug).all()
 
     if(pagecheck.results.length <= 0)
-        return c.text('Bad request', 400)
+        return c.text('Bad request', 400, {
+            'Access-Control-Allow-Origin': 'https://tabby.page'
+        })
 
     // generate an unique comment ID
     const newKey = await crypto.randomUUID()
@@ -128,13 +141,17 @@ app.post('/new/:slug', async (c) => {
     if (result.success) {
         // store requester's address for 5 minutes, for basic spam prevention
         await c.env.ADDRESSES.put(requestIP, 'posted', {expirationTtl: 300})
-        return c.text('OK', 200)
+        return c.text('OK', 200), {
+            'Access-Control-Allow-Origin': 'https://tabby.page'
+        }
     }
     else {
         // something went wrong 3:
         // log it so i can figure out what i messed up
         console.log(result)
-        return c.text('Internal server error', 500)
+        return c.text('Internal server error', 500, {
+            'Access-Control-Allow-Origin': 'https://tabby.page'
+        })
     }
 })
 
